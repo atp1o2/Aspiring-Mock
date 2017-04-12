a21f1a485824aa3105097fa3e455d86b8d77188b
Andrew Pham <andrewpham8@gmail.com>
2017-04-12 21:12:49 +0000

    Add styled-components & Hedron grid

---
+++ src/styles/variables.js
@@ -0,0 +1,18 @@
+const Brand = {
+  yellow: "#F5C600",
+  grey: "#697489",
+  white: "#FFFFFF"
+};
+
+export default Brand ;
+
+/*
+********** How to use in components **********
+
+import Brand from '../../../styles/variables';
+
+const Yellow = styled.button`
+  background-color: ${Brand.yellow};
+  border-color: ${Brand.yellow};
+`;
+*/
