d4e8cedffd199c2a2e851533bc6cf7ceaaf006b9
atp1o2 <andrewpham8@gmail.com>
2017-04-18 20:00:23 -0700

    Install redux, react-redux, react-thunk

--- src/views/student/editProfile/EditStudentProfile.js
+++ src/views/student/editProfile/EditStudentProfile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import EditStudentProfileView from './EditStudentProfileView';
-import Store from '../../../store/store';
 
 class EditStudentProfile extends Component {
   render () {
