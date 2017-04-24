091c424cff14486d4dbbfc7a82319b5e295c84ce
atp1o2 <andrewpham8@gmail.com>
2017-04-23 21:57:48 -0700

    Basic layout for student profile view

--- src/views/student/profile/StudentProfile.js
+++ src/views/student/profile/StudentProfile.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import StudentProfileView from './StudentProfileView';
+import FakeData from '../../../server/upcoming_conversations';
+
+let data = FakeData
 
 class StudentProfile extends Component {
   render () {
     return (
-      <StudentProfileView />
+      <StudentProfileView data={data} />
     );
   }
 }
--- src/views/student/profile/StudentProfileView.js
+++ src/views/student/profile/StudentProfileView.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import ExperienceForm from '../../../components/ExperienceForm/ExperienceForm';
+import ProfileForm from '../../../components/ProfileForm/ProfileForm';
+import CitizenshipForm from '../../../components/CitizenshipForm/CitizenshipForm';
+import EducationForm from '../../../components/EducationForm/EducationForm';
+import AccountForm from '../../../components/AccountForm/AccountForm';
 
 class StudentProfileView extends Component {
   render () {
     return (
-      <div className="container">
-        Profile
-      </div>
+      <Grid>
+        <Row className="text-center">
+          <h1>[first_name] Profile</h1>
+          <hr />
+        </Row>
+        <Row>
+          <Col md={6}>
+            <p className="h2">Status</p>
+            <ProfileForm />
+          </Col>
+          <Col md={6}>
+            <p className="h2">Mini Profile</p>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col md={6}>
+            <p className="h2">Experience</p>
+            <ExperienceForm />
+          </Col>
+          <Col md={6}>
+            <p className="h2">View Experience Cards</p>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col md={6}>
+            <p className="h2">Education</p>
+            <EducationForm />
+          </Col>
+          <Col md={6}>
+            <p className="h2">View Education Cards</p>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col md={6}>
+            <p className="h2">Citizenship</p>
+            <CitizenshipForm />
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col md={6}>
+            <p className="h2">Account Info</p>
+            <AccountForm />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
